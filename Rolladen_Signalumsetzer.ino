1b86a1217eddc78a5a47976a47fd5f5160134764
EArnoldi <37956097+EArnoldi@users.noreply.github.com>
2020-05-09 00:02:25 +0200

    Logik Ablauf beschrieben

--- Rolladen_Signalumsetzer.ino
+++ Rolladen_Signalumsetzer.ino
@@ -23,6 +23,31 @@ void loop() {
   // put your main code here, to run repeatedly:
 
   //Check input here
+  //  Read Pin_1
+  //  If Pin_1 == High        * check if Pin 1 is set
+  //    If flag_Pin_1 == High  * check if Pin 1 was set before
+  //      Count++
+  //    Else
+  //      flag_Pin_1 = High
+  //      Count = 0
+  //      Pin_9 = High
+  //    EndIf
+  //  Else
+  //    If flag_Pin_1 == High
+  //      flag_Pin_1 = Low
+  //      Count = 0
+  //      Pin_9 = High
+  //    Else
+  //      Count++
+  //    EndIf
+  //  EndIf
+  //
+  //  If Count >= 500 msec
+  //    Set Pin_9 Low
+  //  EndIf
+  //
+  //  delay 100 ms
+  //
 
   //Wait for 100ms
   delay(100);
