fdd5d4edee435af239dbbad9b23b4885e959ccda
Alexander Arnoldi <alexander.arnoldi@web.de>
2020-05-08 23:03:10 +0200

    Corrected Input type, use pullups default init output to low

--- Rolladen_Signalumsetzer.ino
+++ Rolladen_Signalumsetzer.ino
@@ -1,18 +1,28 @@
+typed unsigned int Timer_t;
+
+struct PinTimedFlipFlop_t {
+  Timer_t TimerStart;
+};
+
 void setup() {
   // put your setup code here, to run once:
 
-
   //Input Pins:
-  pinMode(1, INPUT);
+  //Use Pullup resistor. To get an input signal, actively pull the line to ground.
+  pinMode(1, INPUT_PULLUP);
 
 
   //Output Pins:
   pinMode(9, OUTPUT);
+  //Set the initial output to low.
+  digitalWrite(9, LOW);
 }
 
+Timer_t Timer;
 void loop() {
   // put your main code here, to run repeatedly:
 
+  //Check input here
 
   //Wait for 100ms
   delay(100);
