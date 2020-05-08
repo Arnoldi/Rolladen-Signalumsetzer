c339c235b68de32d3d55582a634aa4553f1ef94c
Alexander Arnoldi <alexander.arnoldi@web.de>
2020-05-08 20:31:05 +0200

    Erster Eintrag zum Projekt des Signalumsetzers. Das Ziel ist ein Umsetzer, der ein langes Rechtecksignal in zwei kurze Rechtsignale bei den ansteigenden und abfallenden Flanken wandelt.

---
+++ Rolladen_Signalumsetzer.ino
@@ -0,0 +1,18 @@
+#include <TimeInterrupt.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+
+  //Input Pins:
+  pinMode(1, INPUT);
+
+
+  //Output Pins:
+  pinMode(1, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
