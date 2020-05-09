0d364bfa27277835b7621d5d841105300c2f4204
Alexander Arnoldi <alexander.arnoldi@web.de>
2020-05-09 11:38:40 +0200

    Verarbeitungsfunktion für die processPinPairs für die Pinpaare hinzugefügt

--- Rolladen_Signalumsetzer.ino
+++ Rolladen_Signalumsetzer.ino
@@ -1,14 +1,17 @@
+#define PULSE_DURATION 500
+
+
 typedef int Clock_t;
 typedef unsigned int Timer_t;
 typedef unsigned char Pin_t;
 typedef bool PinState_t;
 
-enum class {
-  INPUT_LOW,
+enum FlipFlopState_t {
+  INPUT_LOW = 0,
   INPUT_RISING,
   INPUT_HIGH,
   INPUT_FALLING
-}
+};
 
 struct PinTimedFlipFlop_t {
   Timer_t Timer;
@@ -21,7 +24,6 @@ void setup() {
   // put your setup code here, to run once:
 
   //Input Pins:
-
   //Use Pullup resistor. To get an input signal, actively pull the line to ground.
   pinMode(1, INPUT_PULLUP);
 
@@ -31,8 +33,43 @@ void setup() {
   digitalWrite(9, LOW);
 }
 
-void ProcessPinPair(PinTimedFlipFlop_t PinPair) {
+FlipFlopState_t getNewFlipFlopState(PinState_t OldState, PinState_t NewState) {
+  if (OldState == HIGH) {
+    if (NewState == HIGH) {
+      return INPUT_HIGH;
+    } else {
+      return INPUT_FALLING;
+    }
+  } else {
+    if (NewState == HIGH) {
+      return INPUT_RISING;
+    } else {
+      return INPUT_LOW; 
+    }
+  }
+}
 
+void processPinPair(PinTimedFlipFlop_t PinPair) {
+  PinState_t InputPinNewState = digitalRead(PinPair.InputPin);
+  FlipFlopState_t State = getNewFlipFlopState(PinPair.InputPinPreviousState, InputPinNewState);
+
+  switch (State) {
+    case INPUT_LOW:
+    case INPUT_HIGH:
+      if (PinPair.Timer > 0) {
+        // count timer down
+        PinPair.Timer--;
+      } else {
+        // timer has run out, set output to LOW
+        digitalWrite(PinPair.OutputPin, LOW);
+      }
+    case INPUT_RISING:
+    case INPUT_FALLING:
+      // start pulse by setting the timer to the pulse duration and setting ouput to HIGH
+      PinPair.Timer = PULSE_DURATION;
+      PinPair.InputPinPreviousState = InputPinNewState;
+      digitalWrite(PinPair.OutputPin, HIGH);
+  }
 }
 
 void loop() {
